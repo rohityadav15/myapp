5f937bb10a281162c04b36a363e3a4cdd5c4dbeb
Rohit Yadav <rohit.yadav102002@gmail.com>
2025-08-14 15:52:47 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router";
+
 function Navbar() {
   return (
-    <nav>
-      <h2>I am Navbar</h2>
+    <nav className="bg-amber-300 flex justify-between px-5 items-center">
+      <figure className="font-extrabobold text-2x1">Brouser Router</figure>
+      <ul className="flex gap-5 font-semibold">
+        <li className="p-4">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="p-4">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="p-4">
+          <Link to="/Careers">Careers</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
