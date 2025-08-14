8670c3860e5aec814feee00e03e38040c8ca7c17
Rohit Yadav <rohit.yadav102002@gmail.com>
2025-08-14 15:50:59 +0530

    Error Page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <h1 className='text-center font-bold-text'>NotFound</h1>
+        <figure className='text-center text-5x1' >😒</figure>
+        </div>
+  )
+}
+
+export default NotFound
